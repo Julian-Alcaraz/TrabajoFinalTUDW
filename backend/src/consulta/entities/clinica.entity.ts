912f4941b12a72bb199cfbb235ff0a862113dfcf
Julian-Alcaraz <julianalcaraz4@gmail.com>
2024-10-08 01:31:57 -0300

    Agrego dto de clinica

--- backend/src/consulta/entities/clinica.entity.ts
+++ backend/src/consulta/entities/clinica.entity.ts
@@ -1,14 +1,14 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 import { Consulta } from './consulta.entity';
 
 @Entity()
 export class ClinicaGeneral {
   // esto lo hago para usar el id de la consulta como primaria y foranea
-  @PrimaryGeneratedColumn({ type: 'int' })
+  @PrimaryColumn({ type: 'int' })
   consultaId: number;
 
   @OneToOne(() => Consulta)
-  @JoinColumn()
+  @JoinColumn({ name: 'consultaId' })
   consulta: Consulta;
 
   // datos clinica general
