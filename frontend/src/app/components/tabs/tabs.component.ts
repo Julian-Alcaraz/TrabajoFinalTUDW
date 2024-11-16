90f8adbc2657fbed86661b4f32171b3c36350628
Julian-Alcaraz <julianalcaraz4@gmail.com>
2024-11-16 19:02:05 -0300

    Tabs component

---
+++ frontend/src/app/components/tabs/tabs.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css',
+})
+export class TabsComponent implements OnInit, OnDestroy {
+  @Input() tabs: any[] = [];
+  @Output() positionEmitter: EventEmitter<any> = new EventEmitter<any>();
+  routeSub: any;
+  currentParam: any;
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this._route.queryParams.subscribe((params) => {
+      this.currentParam = +params['i']; // Lógica adicional cuando el parámetro cambia console.log('Parámetro actual:', this.currentParam);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+
+  cambiarParam(value: number) {
+    this._router.navigate([], { relativeTo: this._route, queryParams: { i: value } }); // mantiene otros parámetros de la URL intactos
+    this.positionEmitter.emit(value);
+  }
+}
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { GeneralComponent } from './components/general/general.component';
+import { TabsComponent } from '../../components/tabs/tabs.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [GeneralComponent, TabsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  currentParam: number | null = null;
+
+  escucharParam(value: number) {
+    this.currentParam = value;
+  }
+}
