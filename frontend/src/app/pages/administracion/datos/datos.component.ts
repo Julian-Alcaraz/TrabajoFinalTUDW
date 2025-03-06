7c218e403e43ce1bddfa2105502d683482be6366
Julian-Alcaraz <julianalcaraz4@gmail.com>
2025-03-06 15:50:58 -0300

    Detalle en boton archivo

--- frontend/src/app/pages/administracion/datos/datos.component.ts
+++ frontend/src/app/pages/administracion/datos/datos.component.ts
@@ -1,18 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoadingComponent } from '@app/components/loading/loading.component';
 import { InputFileComponent } from '@components/inputs/input-file.component';
 
 @Component({
   selector: 'app-datos',
   standalone: true,
-  imports: [InputFileComponent, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, InputFileComponent, FormsModule, ReactiveFormsModule, LoadingComponent],
   templateUrl: './datos.component.html',
-  styleUrl: './datos.component.css'
+  styleUrl: './datos.component.css',
 })
 export class DatosComponent {
-  clinicaFileControl: FormControl = new FormControl('',[Validators.required])
-  fonoaudiologiaFileControl: FormControl = new FormControl('',[Validators.required])
-  odontologiaFileControl: FormControl = new FormControl('',[Validators.required])
-  oftalmologiaFileControl: FormControl = new FormControl('',[Validators.required])
+  clinicaFileControl: FormControl = new FormControl('', [Validators.required]);
+  fonoaudiologiaFileControl: FormControl = new FormControl('', [Validators.required]);
+  odontologiaFileControl: FormControl = new FormControl('', [Validators.required]);
+  oftalmologiaFileControl: FormControl = new FormControl('', [Validators.required]);
   // adiccionFileControl: FormControl = new FormControl('',[Validators.required])
+  loading = false;
+
+  cargarArchivoClinica(){
+    this.loading=true
+  }
 }
