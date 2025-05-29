5e7ab189b1472b83ff7bfef4ea1ca70a5408dc6f
Diego966-b <Diegobennjaminn@gmail.com>
2025-05-29 12:42:18 -0300

    Barrios de odontologia

--- backend/src/database/seeders/dev.seeder.ts
+++ backend/src/database/seeders/dev.seeder.ts
@@ -448,6 +448,15 @@ export class DevSeeder implements Seeder {
         { nombre: 'Cinco Saltos', localidad: neuquen },
         { nombre: 'Anahi Mapu', localidad: neuquen },
         { nombre: 'Dvn', localidad: neuquen },
+        // Barrios de planilla odontologia
+        { nombre: 'San Sebastian', localidad: neuquen },
+        { nombre: 'Antartida', localidad: neuquen },
+        { nombre: '22 de febrero', localidad: neuquen },
+        { nombre: 'Ana Mapu', localidad: neuquen },
+        { nombre: 'Bouquet Roldan', localidad: neuquen },
+        { nombre: 'Patagonia', localidad: neuquen },
+        { nombre: 'distrito vecinal', localidad: neuquen },
+        { nombre: 'Paraje El 30', localidad: neuquen },
       ]);
       console.log('Seeding cursos...');
       const cursos = await cursoORM.save([
