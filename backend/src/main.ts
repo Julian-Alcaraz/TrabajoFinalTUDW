c4c638dc9442160c74c3ab2ab44bae4754aada58
Diego966-b <Diegobennjaminn@gmail.com>
2025-06-13 14:08:56 -0300

    Url backend develop

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     // Origenes permitidos: ['http://localhost:4200', 'http://localhost:4500', 'http://localhost:4300', '**']
-    origin: ['http://localhost:3000', 'http://localhost:4200'],
+    origin: ['https://solpatagonia.com.ar'],
     methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS', // Metodos HTTP permitidos
     allowedHeaders: 'Content-Type, Authorization, Content-Disposition', // Encabezados permitidos
     credentials: true, // Permitir el uso de credenciales (cookies, headers de autenticación)
